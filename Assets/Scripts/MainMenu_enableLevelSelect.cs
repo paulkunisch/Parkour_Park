8162ce6b5bb6a4d3201fd61add4ad9da1c740092
Zephyros001 <hasi1999@gmx.at>
2021-04-23 01:37:07 +0200

    Main Menu Overhaul + Scroll Views

---
+++ Assets/Scripts/MainMenu_enableLevelSelect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu_enableLevelSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponent<Button>().interactable = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
