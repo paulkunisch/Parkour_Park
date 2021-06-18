0b6816eeab2309b274d18aef8f5154569fc28080
David Hasenhüttl <hasi1999@gmx.at>
2021-06-18 23:30:11 +0200

    lavalake

---
+++ Assets/Scripts/multiplayerCamara.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class multiplayerCamara : NetworkBehaviour
+{
+    private GameObject ThirdPersonCamera;
+    // Start is called before the first frame update
+    void Update()
+    {
+        ThirdPersonCamera = GameObject.Find("Third Person Camera");
+        if (IsClient)
+        {
+            ThirdPersonCamera.name = "Third Person Camera-c";
+            Debug.Log("Rename tpc" + ThirdPersonCamera.name);
+        }
+
+        if (!IsLocalPlayer)
+        {
+            ThirdPersonCamera.gameObject.SetActive(false);
+        }
+    }
+
+   
+}
