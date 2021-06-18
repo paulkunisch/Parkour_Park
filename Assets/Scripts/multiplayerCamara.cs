32259a845edb49df786083ef7ebb1a9c41a902d7
paul <73228425+paulkunisch@users.noreply.github.com>
2021-06-18 22:31:55 +0200

    multiplayer3

--- Assets/Scripts/multiplayerCamara.cs
+++ Assets/Scripts/multiplayerCamara.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
 
-public class multiplayerCamara : MonoBehaviour
+public class multiplayerCamara : NetworkBehaviour
 {
+    private GameObject ThirdPersonCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ThirdPersonCamera = GameObject.Find("Third Person Camera");
+        if (IsClient)
+        {
+            ThirdPersonCamera.name = "Third Person Camera-c";
+            Debug.Log("Rename tpc" + ThirdPersonCamera.name);
+        }
+
+        if (!IsLocalPlayer)
+        {
+            ThirdPersonCamera.gameObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
