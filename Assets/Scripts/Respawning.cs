f89d0d30961c6acf35cf2e94f0c2708eacbcfc26
paul <73228425+paulkunisch@users.noreply.github.com>
2021-06-26 01:45:57 +0200

    L4

--- Assets/Scripts/Respawning.cs
+++ Assets/Scripts/Respawning.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using MLAPI;
+using MLAPI.Messaging;
 public class Respawning : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
     // Start is called before the first frame update
     void Start()
     {
