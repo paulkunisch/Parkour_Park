39659fe1c663384a062f1712fa17b4187198c7c3
paul <73228425+paulkunisch@users.noreply.github.com>
2021-06-28 21:23:35 +0200

    Light und Disconnect Script

---
+++ Assets/Scripts/DisconnectOnExitMultiplayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MLAPI;
+//Author:Paul Kunisch 
+//Script to close netwokeconections if multiplayer is closed 
+
+public class DisconnectOnExitMultiplayer : NetworkBehaviour
+{
+    public string sceneToGo = "MainMenu";
+
+    public void DisconnectSession(string sceneToGo)
+    {
+        if (IsHost)
+        {
+            Time.timeScale = 1f;
+            NetworkManager.Singleton.StopHost();       
+            SceneManager.LoadScene(sceneToGo);
+            Debug.Log("Sessions Stoped");
+        }
+       
+        if (IsClient)
+        {
+            Time.timeScale = 1f;
+            NetworkManager.Singleton.StopClient();           
+            SceneManager.LoadScene(sceneToGo);
+            Debug.Log("Client Session Stoped");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
